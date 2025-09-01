11d9fc3c150dad25e73ceeb41c5e02adc562caa8
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2025-09-01 12:09:14 +0200

    updated: README

--- cluster-b/run/node1/entrypoint.sh
+++ cluster-b/run/node1/entrypoint.sh
@@ -21,12 +21,23 @@ until curl -s http://127.0.0.1:8200/v1/sys/health >/dev/null; do
   sleep 2
 done
 
-# Solo inicializar si no lo está
-if ! vault status >/dev/null 2>&1; then
+vault_init() {
   echo "Initializing cluster with Transit seal..."
   vault operator init -format json > /vault/data/init.json
 
-  echo "--- ROOT TOKEN: $(jq -r '.root_token' /vault/data/init.json)"
+  # limpiamos el token para el unseal
+  unset VAULT_TOKEN
+}
+
+# Solo inicializar si no lo está
+# Este paso solo se ejecuta la primera vez que se levanta el cluster
+if ! vault status >/dev/null 2>&1; then
+  vault_init
+
+  # utilizarlo para login temporal
+  VAULT_TOKEN="$(jq -r '.root_token' /vault/data/init.json)"
+
+  echo "--- ROOT TOKEN: VAULT_TOKEN"
 fi
 
 wait $VAULT_PID
