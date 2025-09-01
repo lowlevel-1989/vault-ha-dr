bac2e8a8d3f23b64f63e25c0675ea17b80988908
davidaram <davidaroca27@gmail.com>
2025-09-01 20:49:50 +0200

    Enable DR on cluster C-D

--- cluster-c/run/node1/entrypoint.sh
+++ cluster-c/run/node1/entrypoint.sh
@@ -29,6 +29,27 @@ vault_init() {
   unset VAULT_TOKEN
 }
 
+vault_dr_enable() {
+  # VAULT_TOKEN como login temporal
+  # REF: https://developer.hashicorp.com/vault/tutorials/enterprise/disaster-recovery#enable-dr-primary-replicationense
+
+  export VAULT_TOKEN=$1
+  echo --- DR[1]. Enable DR replication on the primary cluster.
+  sleep 2
+  vault write -f sys/replication/dr/primary/enable
+
+  echo --- DR[2]. Generate a secondary token.
+  sleep 2
+  RESPONSE=$(vault write --format json sys/replication/dr/primary/secondary-token id="dr-secondary")
+  SECONDARY_TOKEN=$(echo "$RESPONSE" | jq -r ".wrap_info.token")
+  echo $SECONDARY_TOKEN > /vault/shared/cluster_c_wrapping_token
+  touch /vault/shared/cluster_c_wrapping_token_ready
+
+  echo secondary-token: $SECONDARY_TOKEN
+
+  unset VAULT_TOKEN
+}
+
 # Solo inicializar si no lo está
 # Este paso solo se ejecuta la primera vez que se levanta el cluster
 if ! vault status >/dev/null 2>&1; then
@@ -38,6 +59,8 @@ if ! vault status >/dev/null 2>&1; then
   VAULT_TOKEN="$(jq -r '.root_token' /vault/data/init.json)"
 
   echo "--- ROOT TOKEN: $VAULT_TOKEN"
+
+  vault_dr_enable $VAULT_TOKEN
 fi
 
 wait $VAULT_PID
