a5aa9385fb5e37bc0ee6014d7ba9251c3e705732
davidaram <davidaroca27@gmail.com>
2025-09-03 07:56:55 +0200

    Enable Performance Replication not working in cluster C

--- cluster-c/run/node1/entrypoint.sh
+++ cluster-c/run/node1/entrypoint.sh
@@ -29,6 +29,25 @@ vault_init() {
   unset VAULT_TOKEN
 }
 
+vault_pr_enable() {
+  SEC_ADDR=http://vaultC-1:8200
+
+  until [ -f "/vault/shared/cluster_a_performance_token_ready" ]; do
+    echo "Waiting for cluster_a_performance_token from cluster A..."
+    sleep 4
+  done
+
+  echo --- PR[1]Enabling PR replication on cluster C.
+
+  export VAULT_TOKEN=$1
+  SECONDARY_PR_TOKEN=$(cat /vault/shared/cluster_a_performance_token)
+
+  vault write -address=$SEC_ADDR sys/replication/performance/secondary/enable token="$SECONDARY_PR_TOKEN"
+
+  unset VAULT_TOKEN
+  unset SEC_ADDR
+}
+
 vault_dr_enable() {
   # VAULT_TOKEN como login temporal
   # REF: https://developer.hashicorp.com/vault/tutorials/enterprise/disaster-recovery#enable-dr-primary-replicationense
@@ -50,17 +69,23 @@ vault_dr_enable() {
   unset VAULT_TOKEN
 }
 
+
+
 # Solo inicializar si no lo está
 # Este paso solo se ejecuta la primera vez que se levanta el cluster
 if ! vault status >/dev/null 2>&1; then
   vault_init
 
   # utilizarlo para login temporal
-  VAULT_TOKEN="$(jq -r '.root_token' /vault/data/init.json)"
+  INIT_TOKEN="$(jq -r '.root_token' /vault/data/init.json)"
 
-  echo "--- ROOT TOKEN: $VAULT_TOKEN"
+  echo "--- ROOT TOKEN: $INIT_TOKEN"
 
-  vault_dr_enable $VAULT_TOKEN
+  vault_pr_enable $INIT_TOKEN
+  vault_dr_enable $INIT_TOKEN
+  
+  unset INIT_TOKEN
+
 fi
 
 wait $VAULT_PID
